bac909ac50c2a7605c6bad24835f10f4d021adeb
hahahatt <shonsm1998@naver.com>
2025-07-03 17:24:36 +0900

    메인페이지에 있어도 홈 클릭 시 새로고침 되게 변경

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import '../css/Sidebar.css';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
+
+  const location = useLocation();
+
+  const handleHomeClick = (e) => {
+    if (location.pathname === '/') {
+      e.preventDefault();
+      window.location.reload(); // 전체 새로고침
+    }
+  };
+
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
       <ul>
-        <li><Link to="/">홈</Link></li>
+        <li><Link to="/" onClick={handleHomeClick}>홈</Link></li>
         <li><Link to="/graph">그래프</Link></li>
       </ul>
     </div>
